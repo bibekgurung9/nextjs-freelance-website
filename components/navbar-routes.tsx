3951e6ab7f6ef92836197d954e5eb4319b202764
Bibek Gurung <100699318+bibekgurung9@users.noreply.github.com>
2024-01-25 23:54:51 +0545

    added search query

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -5,15 +5,23 @@ import { usePathname} from "next/navigation"
 import { Button } from "./ui/button"
 import { LogOut } from "lucide-react"
 import Link from "next/link"
+import { SearchInput } from "./search-input"
 
 export const NavbarRoutes = () => {
   const pathname = usePathname()
 
   const isClientPage = pathname?.startsWith("/client");
   const isJobPage = pathname?.includes("/job");
+  const isSearchPage = pathname === "/search";
 
 
   return(
+    <>
+    {isSearchPage && (
+      <div className="hidden md:block">
+        <SearchInput />
+      </div>
+    )}
     <div className="flex gap-x-2 ml-auto">
       {isClientPage || isJobPage ? (
         <Link href="/">
@@ -30,5 +38,6 @@ export const NavbarRoutes = () => {
       <UserButton
         afterSignOutUrl="/" />
     </div>
+    </>
   )
 }
