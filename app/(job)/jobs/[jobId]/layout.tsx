ed85e29e927fd52bb7b1aea9ad4a9d5d4bf3593a
Bibek Gurung <100699318+bibekgurung9@users.noreply.github.com>
2024-02-21 23:37:34 +0545

    added jobs ui

--- app/(job)/jobs/[jobId]/layout.tsx
+++ app/(job)/jobs/[jobId]/layout.tsx
@@ -2,7 +2,10 @@ import { auth } from "@clerk/nextjs";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { getProgress } from "@/actions/get-progress";
-import JobSidebar from "./_components/job-sidebar";
+import { Navbar } from "@/app/(frontend)/_components/navbar";
+
+import { FaMoneyBill, FaAudioDescription } from "react-icons/fa";
+import { ZodDate } from "zod";
 
 const JobLayout = async ({
     children,
@@ -49,13 +52,19 @@ const JobLayout = async ({
 
     return (
       <div className="h-full">
-        <div>
-          <h1>{job.title}</h1>
+        <div className="h-[70px] md:pl-56 fixed inset-y-0 w-full z-50 bg-sky-500">
+          <Navbar /> 
+        </div>
+        <div className="mt-20 flex flex-col gap-y-4 px-4">
+          <h1 className="text-4xl font-bold">{job.title}</h1>
+          <span className="text-slate-400 text-xl">Posted On: {job.createdAt.toLocaleDateString()}</span>
+          <span className="text-slate-400 text-xl">Posted By: {job.userId}</span>
+          <p className="text-2xl"><span className="font-bold">Job Description</span>:<br/>{job.description}</p>
+          <p className="text-2xl"><span className="font-bold"><FaMoneyBill /> Budget: </span>{job.price}</p>
         </div>    
         <main className="h-full">
           {children}
         </main>
-        
       </div>
     )
   }
--- app/(job)/jobs/[jobId]/page.tsx
+++ app/(job)/jobs/[jobId]/page.tsx
@@ -1,8 +1,52 @@
+"use client";
 import React from 'react'
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Enter Your Name To Be Seen By Client.",
+  }).max(50),
+  descripton: z.string().min(1, {
+    message: "Description Cannot Be Empty",
+  }).max(255),
+  extraLinks: z.string().min(2, {
+    message: "Enter Links to your past projects or portfolio",
+  }),
+})
 
 const JobIdPage = () => {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      description: "",
+      extraLinks: "",
+    },
+  })
+
+
   return (
-    <div>JobIdPage</div>
+    <div>
+      {/* <Form {...form}>
+
+      </Form> */}
+
+    </div>
   )
 }
 
